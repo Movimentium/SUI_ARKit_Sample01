b5c1722e6315248cc77cd821414052dc4391493c
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-06-18 11:12:48 +0200

    Initial Commit

---
+++ SUI_ARKit_Sample01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SUI_ARKit_Sample01
+//
+//  Created by Miguel Gallego on 18/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SUI_ARKit_Sample01/SUI_ARKit_Sample01App.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_ARKit_Sample01App.swift
+//  SUI_ARKit_Sample01
+//
+//  Created by Miguel Gallego on 18/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SUI_ARKit_Sample01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
